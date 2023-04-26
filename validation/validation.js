f3cc8e5fe48185fafbb14d6e042ce1af5a5f473f
Elena Fedotova <e-a-fedotova@yandex.ru>
2023-04-26 14:26:52 +0300

    fix validation

--- validation/validation.js
+++ validation/validation.js
@@ -20,7 +20,7 @@ const createUserValidation = celebrate({
   body: Joi.object().keys({
     email: Joi.string().email().required(),
     password: Joi.string().required(),
-    name: Joi.string().min(2).max(30),
+    name: Joi.string().min(2).max(30).required(),
   }),
 });
 
